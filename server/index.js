104a0cdb7a501b9d6c3c29ca195fee84255daad9
Daniel Kim <nautilus@Nautilus.local>
2019-08-26 10:44:05 -0700

    Add styles.css file and stub out REST API routes for express server

--- server/index.js
+++ server/index.js
@@ -10,4 +10,20 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+  // TODO: build get request handler
+});
+
+app.post('/', (req, res) => {
+  // TODO: build post handler
+});
+
+app.delete('/', (req, res) => {
+  // TODO: build delete handler
+});
+
+app.put('/', (req, res) => {
+  // TODO: build put handler
+});
+
 app.listen(PORT, () => console.log(`Express server running Creature on port ${PORT}`));
